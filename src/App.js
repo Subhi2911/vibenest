b027c12e595d873cd305102189772344a4c6e70c
Subhasmita Mishra <msubhasmita29@gmail.com>
2025-07-16 18:29:06 +0530

    solving errors

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
     };
     return (
         <div >
-            <BrowserRouter basename='/'>
+            <BrowserRouter >
                 <BlogState>
                     <Navbar />
                     <Alert alert={alert}/> 
